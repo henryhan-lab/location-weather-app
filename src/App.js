01004daabefb6e93c16c8cb84098c74e4f3b5743
Rui Han <henryhan0415@gmail.com>
2022-07-06 13:12:41 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import "./App.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { LayersControl } from "react-leaflet/LayersControl";
+import { geocodeByAddress, getLatLng } from "react-google-places-autocomplete";
+import ReactWeather, { useOpenWeather } from "react-open-weather";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+function App() {
+  const center = [51.505, -0.09];
+  const [location, setLocation] = useState(null);
+  const [map, setMap] = useState(null);
+  const startingPosition = { lat: 40.7357, lng: -74.1724 };
+  const [lnglat, setLnglat] = useState(startingPosition);
+  const openWeatherKey = '058d0a4d8e0854cbd38a034edce29b3e';
+  const googlePlacesKey = 'AIzaSyDgWbaJbfYaTV3XpQ9OF1iL8wNfHcXrkh0'
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: openWeatherKey,
+    lat: lnglat.lat,
+    lon: lnglat.lng,
+    lang: "en",
+    unit: "metric",
+  });
+  useEffect(() => {
+    if (location != null) {
+      geocodeByAddress(location?.value?.description)//geocode address and updates value to latlng each time location, lnglat changes 
+        .then((results) => getLatLng(results[0]))
+        .then(({ lat, lng }) => setLnglat({ lat, lng }));
+    }
+  }, [location, lnglat]);
+  useEffect(() => {
+    if (map != null) {
+      map.setView(lnglat, 10);//updates the view of map each time map, lnglat changes 
+    }
+  }, [map, lnglat]);
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <MapContainer
+          center={center}
+          zoom={13}
+          scrollWheelZoom={false}
+          ref={setMap}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <LayersControl position="topright"> 
+            <LayersControl.Overlay name="Temperature">
+              <TileLayer url="https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=058d0a4d8e0854cbd38a034edce29b3e" />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Wind Speed">
+              <TileLayer url="https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=058d0a4d8e0854cbd38a034edce29b3e" />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Clouds ">
+              <TileLayer url="https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=058d0a4d8e0854cbd38a034edce29b3e" />
+            </LayersControl.Overlay>
+          </LayersControl>
+        </MapContainer>
+        <div style={{ width: "40%" }}>
+          <GooglePlacesAutocomplete
+            apiKey={googlePlacesKey}
+            selectProps={{
+              location,
+              onChange: setLocation,
+            }}
+          />
+          <ReactWeather
+            isLoading={isLoading}
+            errorMessage={errorMessage}
+            data={data}
+            lang="en"
+            locationLabel={
+              location ? location?.value?.description : "New Jersey"
+            }
+            unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
+            showForecast
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+export default App;
